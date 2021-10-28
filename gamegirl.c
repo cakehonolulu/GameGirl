b9c85b80db533c8ab84221d5bba214d11dfb7550
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-28 21:50:13 +0200

    Move MMU checks over mmu.c

--- gamegirl.c
+++ gamegirl.c
@@ -7,13 +7,4 @@ int main(int argc, char **argv)
 
 	// Declare a new MMU
 	gb_mmu_t mmu;
-
-	if (sizeof(mmu.gb_mmap) != GB_ADDRSPC_SIZE)
-	{
-		printf("MMU Size: 0x%lx\n", sizeof(mmu.gb_mmap));
-	} else {
-		printf("MMU Size not correct, expected 0x10000 got 0x%lu instead! Exiting...\n", sizeof(mmu.gb_mmap));
-		return EXIT_FAILURE;
-	}
-	
 }
