94e7fe53e3079114786b718bf3f70ba3df4b54c8
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-28 12:22:37 +0200

    GameGirl: Initial Bringup

---
+++ gamegirl.c
@@ -0,0 +1,15 @@
+#include "includes/gamegirl.h"
+
+int main(int argc, char **argv)
+{
+	printf("GameGirl - A C-21 Multiplatform Game Boy Emulator\n");
+
+	// Check for commandline arguments
+	if (argc < 2)
+	{
+		printf("Usage: ./gamegirl [progname]\n");
+		return EXIT_FAILURE;
+	}
+
+
+}
