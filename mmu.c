d3debc070088ca07a7e4f62d1a9ac27473064d64
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-28 21:58:59 +0200

    MMU: Start cleaning up everything

--- mmu.c
+++ mmu.c
@@ -5,14 +5,21 @@ gb_mmu_t *mmu_init()
 {
 	gb_mmu_t *gb_mmu = (gb_mmu_t*)malloc(sizeof(gb_mmu_t));
 
-	if (sizeof(gb_mmu.gb_mmap) != GB_ADDRSPC_SIZE)
+	if (!gb_mmu)
 	{
-		printf("MMU Size: 0x%lx\n", sizeof(gb_mmu.gb_mmap));
+		printf("MMU Structure can't be allocated, exiting...");
+	}
+
+	if (sizeof(gb_mmu->gb_mmap) == GB_ADDRSPC_SIZE)
+	{
+		printf("MMU Size: 0x%lx\n", sizeof(gb_mmu->gb_mmap));
 	} else {
-		printf("MMU Size not correct, expected 0x10000 got 0x%lu instead! Exiting...\n", sizeof(mmu.gb_mmap));
+		printf("MMU Size not correct, expected 0x10000 got 0x%lu instead! Exiting...\n",
+		 sizeof(gb_mmu->gb_mmap));
+
 		return EXIT_FAILURE;
 	}
-	
+
 	gb_mmu->gb_bootrom_end = gb_mmu->gb_address_space + 0xFF50;
 
 	return gb_mmu;
