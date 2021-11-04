013eab4cc700b9947b18cc1ba69a9e8d66cbd42d
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-04 19:49:03 +0100

    More PPU work, emulator now keeps going

--- gpu.c
+++ gpu.c
@@ -1,4 +1,5 @@
 #include "include/gpu.h"
+#include "include/int.h"
 
 static uint64_t m_gpu_elapsed_ticks = 0;
 
@@ -15,15 +16,55 @@ void m_gpu_step()
 	switch (gpu.m_cgpu_mode)
 	{
 		case M_GPU_HBLANK:
+			if (gpu.m_ticks >= 204)
+			{
+				gpu.m_scanline++;
+
+				if (gpu.m_scanline == 143)
+				{
+					if (ints.m_enabled & INT_VBLANK)
+					{
+						ints.m_flags |= INT_VBLANK;
+						gpu.m_cgpu_mode = M_GPU_VBLANK;
+					} else {
+						gpu.m_cgpu_mode = M_GPU_OAM;
+						gpu.m_ticks -= 204;
+					}
+				}
+			}
 			break;
 
 		case M_GPU_VBLANK:
+			if (gpu.m_ticks >= 456)
+			{
+				gpu.m_scanline++;
+
+				if (gpu.m_scanline > 153)
+				{
+					gpu.m_cgpu_mode = M_GPU_OAM;
+					gpu.m_scanline = 0;
+				}
+
+				gpu.m_ticks -= 456;
+			}
 			break;
 
 		case M_GPU_OAM:
+			if (gpu.m_ticks >= 80)
+			{
+				gpu.m_cgpu_mode = M_GPU_VRAM;
+				
+				gpu.m_ticks -= 80;
+			}
 			break;
 
 		case M_GPU_VRAM:
+			if (gpu.m_ticks >= 172)
+			{
+				gpu.m_cgpu_mode = M_GPU_HBLANK;
+
+				gpu.m_ticks -= 172;
+			}
 			break;
 
 		default:
--- include/int.h
+++ include/int.h
@@ -1,6 +1,12 @@
 #include <stdint.h>
 #include "mmu.h"
 
+#define INT_VBLANK	(1 << 0)
+#define INT_LCD		(1 << 1)
+#define INT_TIMER	(1 << 2)
+#define INT_SERIAL	(1 << 3)
+#define INT_JOYPAD	(1 << 4)
+
 typedef struct m_int
 {
 	uint8_t m_master;
