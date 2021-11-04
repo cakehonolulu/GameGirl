e4100a92d739e68097be481b14e06633c30d3802
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-04 18:55:28 +0100

    gpu.h: More defines

--- include/gpu.h
+++ include/gpu.h
@@ -12,3 +12,15 @@ enum {
 	M_GPU_OAM = 2,
 	M_GPU_VRAM = 3
 };
+
+#define M_DMG01_ROWS 144
+#define M_DMG01_COLS 160
+
+// Declare a structure with the PPU's foundations
+// DMG-01's Screen's Resolution equals to 160 x 144 (px)
+typedef struct m_gpu
+{
+	uint8_t m_scanline;				// Current scanline (DMG-01 imitates a CRT monitor)
+	uint8_t m_lateralscroll;		// X coordinate (Horitzontal, 160px-wide)
+	uint8_t m_verticalscroll;		// Y Coordinate (Vertical, 144px-wide)
+};
