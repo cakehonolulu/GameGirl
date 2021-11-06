3dc85227542ad794d91c309e02575507c9eb428e
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-06 20:17:28 +0100

    Disable int in bootstrap

--- gpu.c
+++ gpu.c
@@ -16,18 +16,19 @@ void m_gpu_step()
 		case M_GPU_HBLANK:
 			if (gpu.m_ticks >= 204)
 			{
+				printf("LY: 0x%02X\n", mmu_read_byte(0xFF44));
 				gpu.m_scanline++;
 
 				if (gpu.m_scanline == 143)
 				{
-					if (ints.m_enabled & INT_VBLANK)
+					/*if (ints.m_enabled & INT_VBLANK)
 					{
-						ints.m_flags |= INT_VBLANK;
+						ints.m_flags |= INT_VBLANK;*/
 						gpu.m_cgpu_mode = M_GPU_VBLANK;
-					} else {
+					/*} else {
 						gpu.m_cgpu_mode = M_GPU_OAM;
 						gpu.m_ticks -= 204;
-					}
+					}*/
 				}
 			}
 			break;
@@ -35,7 +36,7 @@ void m_gpu_step()
 		case M_GPU_VBLANK:
 			if (gpu.m_ticks >= 456)
 			{
-				gpu.m_scanline++;
+				gpu.m_scanline++;		
 
 				if (gpu.m_scanline > 153)
 				{
