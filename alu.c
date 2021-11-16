23ffd71d66ad8e4ec6cb09ae4fc1d6cd145f7943
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-16 18:31:13 +0100

    Introduce alu, a submodule built for GameBoy's arithmetic operations

---
+++ alu.c
@@ -0,0 +1,54 @@
+#include <alu.h>
+
+/*
+	alu.c
+
+	This file contains the source code that implements basic
+	arithmetic-logic-unit tasks (Register increment, decrement...)
+	to enhance code readability and simplify code.
+*/
+
+// Register increment (Lone-pair-register exclusive)
+uint8_t increment(uint8_t m_register)
+{
+	// Check for undefined behaviour
+	if (m_register == NULL)
+	{
+		alu_error("Detected an undefined register!");
+		return 1;
+	}
+
+	/*
+		Check if we need to enable half-carry bit
+		Mask the lower nibble of the register and
+		compare it with 0b00001111
+	*/
+	if ((m_register & 0b00001111) == 0b00001111)
+	{
+		// Set the Half-Carry bit
+		FLAG_SET(HALF);
+	}
+	else
+	{
+		// Clear the Half-Carry bit
+		FLAG_UNSET(HALH);
+	}
+
+	// Increment the register value
+	m_register++;
+
+	// Check if register value is not zero
+	if (m_register != 0)
+	{
+		// Unset the Zero (Result) flag
+		FLAG_UNSET(ZERO);
+	}
+	else
+	{
+		// Set the Zero (Result) flag
+		FLAG_SET(ZERO);
+	}
+
+	// Finally, clear the negative flag as we're dealing with increments
+	FLAG_UNSET(NGTV);
+}
---
+++ include/alu.h
@@ -0,0 +1,18 @@
+#include <gamegirl.h>
+
+// ALU Log-level types
+enum alu_loglevels
+{
+	NORMAL = 0,
+	WARNING = 1,
+	ERROR = 2
+};
+
+// Arithmetic-Logic-Unit Special Messaging Macros
+#define alu_message(loglevel, msg, ...) do { if (loglevel == NORMAL) { printf("[ALU] %s\n", msg); } else if (loglevel == ERROR) { printf("[ALU] \033[0;31m%s\033[0;0m\n", msg); } } while (0)
+
+// ALU Normal Message
+#define alu_printf(msg) alu_message(NORMAL, msg)
+
+// ALU Error Message
+#define alu_error(msg) alu_message(ERROR, msg)
