a1ee2371296156abd6cdf467c9e0089db380731f
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-23 11:16:38 +0100

    Change ALU Loglevel Enum Names

--- include/alu.h
+++ include/alu.h
@@ -3,19 +3,19 @@
 // ALU Log-level types
 enum alu_loglevels
 {
-	NORMAL = 0,
-	WARNING = 1,
-	ERR = 2
+	ALU_NORMAL = 0,
+	ALU_WARNING = 1,
+	ALU_ERROR = 2
 };
 
 // Arithmetic-Logic-Unit Special Messaging Macros
-#define alu_message(loglevel, msg, ...) do { if (loglevel == NORMAL) { printf("[ALU] %s\n", msg); } else if (loglevel == ERROR) { printf("[ALU] \033[0;31m%s\033[0;0m\n", msg); } } while (0)
+#define alu_message(loglevel, msg, ...) do { if (loglevel == ALU_NORMAL) { printf("[ALU] %s\n", msg); } else if (loglevel == ALU_ERROR) { printf("[ALU] \033[0;31m%s\033[0;0m\n", msg); } } while (0)
 
 // ALU Normal Message
-#define alu_printf(msg) alu_message(NORMAL, msg)
+#define alu_printf(msg) alu_message(ALU_NORMAL, msg)
 
 // ALU Error Message
-#define alu_error(msg) alu_message(ERROR, msg)
+#define alu_error(msg) alu_message(ALU_ERROR, msg)
 
 // Increment a 8-bit register
 uint8_t increment(uint8_t m_register);
