b99747234d9bde4c042aa3e1e2821231512a1265
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-11-29 12:46:36 +0100

    dbg: Poll from terminal instead of SDL window

--- debugger.c
+++ debugger.c
@@ -4,14 +4,19 @@
 #include <debugger.h>
 #include <cb_opcodes.h>
 #include <ppu.h>
+#include <signal.h>
 
 #define NEXT_BYTE m_dmg->m_cpu->m_boperand
 #define NEXT_WORD m_dmg->m_cpu->m_woperand
 
+void m_exit(int m_unused)
+{
+	(void) m_unused;
+	SDL_Quit();
+}
+
 int m_run_debugger(m_dmg_t *m_dmg)
 {
-	SDL_Event m_dbgevent;
-
 	printf("\e[1;1H\e[2J");
 	printf("\033[1;32mEntered Debugging Step Mode!\033[0;0m");
 
@@ -20,6 +25,8 @@ int m_run_debugger(m_dmg_t *m_dmg)
 	m_printregs(m_dmg);
 	
 	printf("Legend: \033[0;34mPrevious Instruction\033[0;0m, \033[0;33mCurrent Instruction\033[0;0m\n\n");
+
+	signal(SIGINT, m_exit);
 
 	// Check if we're on the start of the BootROM (Don't print previous opcode as it doesn't exist)
 	if (PC)
@@ -90,102 +97,80 @@ int m_run_debugger(m_dmg_t *m_dmg)
 
 	while (true)
 	{
-		while (SDL_PollEvent(&m_dbgevent))
+		printf("\e[1;1H\e[2J");
+		m_dmg->m_cpu->prev_pc = PC;
+
+		size_t m_cycles = m_exec(m_dmg);
+		m_ppu_step(m_dmg, m_cycles);
+		m_interrupt_check(m_dmg);
+
+		m_printregs(m_dmg);
+
+		printf("Legend: \033[0;34mPrevious Instruction\033[0;0m, \033[0;33mCurrent Instruction\033[0;0m\n\n");
+
+		uint8_t m_dbgopc = READB(PC);
+
+		if (m_dmg->m_cpu->m_opcode != 0xCB)
 		{
-			if (m_dbgevent.type == SDL_QUIT)
+			if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 0)
 			{
-				goto exit;
+				printf("\033[0;34m00:%04X  %02X   \033[0;0m       %s\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr);
 			}
-
-			if (m_dbgevent.type == SDL_KEYDOWN)
+			else
+			if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 1)
 			{
-				if (m_dbgevent.key.keysym.sym == SDLK_RETURN)
+				if ((m_dmg->m_cpu->m_opcode == 0x18) | (m_dmg->m_cpu->m_opcode == 0x20) | (m_dmg->m_cpu->m_opcode == 0x28))
 				{
-					printf("\e[1;1H\e[2J");
-
-					m_dmg->m_cpu->prev_pc = PC;
-
-					size_t m_cycles = m_exec(m_dmg);
-					m_ppu_step(m_dmg, m_cycles);
-					m_interrupt_check(m_dmg);
-
-					m_printregs(m_dmg);
-
-					printf("Legend: \033[0;34mPrevious Instruction\033[0;0m, \033[0;33mCurrent Instruction\033[0;0m\n\n");
-
-					uint8_t m_dbgopc = READB(PC);
-
-					if (m_dmg->m_cpu->m_opcode != 0xCB)
-					{
-						if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 0)
-						{
-							printf("\033[0;34m00:%04X  %02X   \033[0;0m       %s\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr);
-						}
-						else
-						if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 1)
-						{
-							if ((m_dmg->m_cpu->m_opcode == 0x18) | (m_dmg->m_cpu->m_opcode == 0x20) | (m_dmg->m_cpu->m_opcode == 0x28))
-							{
-								printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s$%04X (%d)\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, ((m_dmg->m_cpu->prev_pc + 2) + (int8_t) READB(m_dmg->m_cpu->prev_pc + 1)),(int8_t) READB(m_dmg->m_cpu->prev_pc + 1));
-							}
-							else
-							{
-								printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s$%02X\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, READB(m_dmg->m_cpu->prev_pc + 1));
-							}
-						}
-						else
-						if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 2)
-						{
-							printf("\033[0;34m00:%04X  %02X %02X %02X\033[0;0m    %s$%04X\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), READB(m_dmg->m_cpu->prev_pc + 2), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, ((READB(m_dmg->m_cpu->prev_pc + 2) << 8) | (READB(m_dmg->m_cpu->prev_pc + 1))));
-						}
-					}
-					else
-					{
-						// Extended (0xCB) Opcode Handling
-						printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr_cb[READB(m_dmg->m_cpu->prev_pc + 1)].m_instr);
-					}
-
-					if (m_dbgopc != 0xCB)
-					{
-						/* Enter Debug Second Opcode (Current) */
-						if (m_gb_instr[m_dbgopc].m_operand == 0)
-						{
-							printf("\033[0;33m00:%04X  %02X  	  ->\033[0;0m %s\n\n", PC, READB(PC), m_gb_instr[m_dbgopc].m_instr);
-						}
-						else
-						if (m_gb_instr[m_dbgopc].m_operand == 1)
-						{
-							if ((m_dbgopc == 0x18) | (m_dbgopc == 0x20) | (m_dbgopc == 0x28))
-							{
-								printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s$%04X (%d)\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr[m_dbgopc].m_instr, ((PC + 2) + (int8_t) READB(PC + 1)),(int8_t) READB(PC + 1));
-							}
-							else
-							{
-								printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s$%02X\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr[m_dbgopc].m_instr, READB(PC + 1));
-							}
-						}
-						else
-						if (m_gb_instr[m_dbgopc].m_operand == 2)
-						{
-							printf("\033[0;33m00:%04X  %02X %02X %02X ->\033[0;0m %s$%04X\n\n", PC, READB(PC), READB(PC + 1), READB(PC + 2), m_gb_instr[m_dbgopc].m_instr, ((READB(PC + 2) << 8) | (READB(PC + 1))));
-						}
-					}
-					else
-					{
-						printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr_cb[READB(PC + 1)].m_instr);
-					}
-							
-					printf("Press Enter to Step...\n");
+					printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s$%04X (%d)\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, ((m_dmg->m_cpu->prev_pc + 2) + (int8_t) READB(m_dmg->m_cpu->prev_pc + 1)),(int8_t) READB(m_dmg->m_cpu->prev_pc + 1));
+				}
+				else
+				{
+					printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s$%02X\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, READB(m_dmg->m_cpu->prev_pc + 1));
 				}
 			}
+			else
+			if (m_gb_instr[m_dmg->m_cpu->m_opcode].m_operand == 2)
+			{
+				printf("\033[0;34m00:%04X  %02X %02X %02X\033[0;0m    %s$%04X\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), READB(m_dmg->m_cpu->prev_pc + 2), m_gb_instr[m_dmg->m_cpu->m_opcode].m_instr, ((READB(m_dmg->m_cpu->prev_pc + 2) << 8) | (READB(m_dmg->m_cpu->prev_pc + 1))));
+			}
 		}
+		else
+		{
+			// Extended (0xCB) Opcode Handling
+			printf("\033[0;34m00:%04X  %02X %02X      \033[0;0m %s\n", m_dmg->m_cpu->prev_pc, READB(m_dmg->m_cpu->prev_pc), READB(m_dmg->m_cpu->prev_pc + 1), m_gb_instr_cb[READB(m_dmg->m_cpu->prev_pc + 1)].m_instr);
+		}
+
+		if (m_dbgopc != 0xCB)
+		{
+			/* Enter Debug Second Opcode (Current) */
+			if (m_gb_instr[m_dbgopc].m_operand == 0)
+			{
+				printf("\033[0;33m00:%04X  %02X  	  ->\033[0;0m %s\n\n", PC, READB(PC), m_gb_instr[m_dbgopc].m_instr);
+			}
+			else
+			if (m_gb_instr[m_dbgopc].m_operand == 1)
+			{
+				if ((m_dbgopc == 0x18) | (m_dbgopc == 0x20) | (m_dbgopc == 0x28))
+				{
+					printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s$%04X (%d)\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr[m_dbgopc].m_instr, ((PC + 2) + (int8_t) READB(PC + 1)),(int8_t) READB(PC + 1));
+				}
+				else
+				{
+					printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s$%02X\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr[m_dbgopc].m_instr, READB(PC + 1));
+				}
+			}
+			else
+			if (m_gb_instr[m_dbgopc].m_operand == 2)
+			{
+				printf("\033[0;33m00:%04X  %02X %02X %02X ->\033[0;0m %s$%04X\n\n", PC, READB(PC), READB(PC + 1), READB(PC + 2), m_gb_instr[m_dbgopc].m_instr, ((READB(PC + 2) << 8) | (READB(PC + 1))));
+			}
+		}
+		else
+		{
+			printf("\033[0;33m00:%04X  %02X %02X    ->\033[0;0m %s\n\n", PC, READB(PC), READB(PC + 1), m_gb_instr_cb[READB(PC + 1)].m_instr);
+		}
+							
+		printf("Press Enter to Step...\n");
+		getchar();
 	}
-
-exit:
-	// Free MMU data
-	mmu_halt(m_dmg);
-
-	SDL_Quit();
-
-	return EXIT_SUCCESS;
 }
